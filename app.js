59be22c7f2f6e322ee170803320889fa139d337d
Josefina Pereyra <j08pereyra@gmail.com>
2024-04-02 18:13:17 -0300

    Segunda Entrega

--- app.js
+++ app.js
@@ -1,53 +1,136 @@
+const fs = require('fs').promises
+
 class productManager {
     constructor (){
-        this.products = [];
+        this.productsFile = 'productos.json';
     }
 
-    addProduct(title, description, price, thumbnail, code, stock){
-        const codigoEncontrado= this.products.find((product)=> product.code === code);
-        if(codigoEncontrado){
-            console.log('El codigo ya existe');
-            return;
+    async leerProductos (){
+        try{
+            const productos = await fs.readFile(this.productsFile, 'utf8');
+            return JSON.parse(productos);
+        } catch (error){
+            if (error.code === 'ENOENT'){
+                return [];
+            }else{
+                throw error;
+            }
         }
+    }
+
+    async addProduct(product){
+        try {
+            let productos = await this.leerProductos();
+            const codigoEncontrado = productos.find((producto) => producto.code === product.code);
+            if (codigoEncontrado) {
+                console.log('El codigo ya existe');
+                return;
+            }
+            if (!product.title || !product.description || !product.price || !product.thumbnail || !product.code || !product.stock) {
+                console.log('debe completar todos los datos');
+                return;
+            }
+            const idProducto = productos.length + 1;
+            const producto = { ...product, id: idProducto };
+            productos.push(producto);
+            await fs.writeFile(this.productsFile, JSON.stringify(productos, null, 2));
+        } catch (error) {
+            console.log('error al agregar producto', error);
+        }
+    }
+
+    async getProducts(){
+        try {
+            return await this.leerProductos()
+        } catch (error) {
+            console.error("Error al consultar productos", error)
+            return []
+        }
+    }
+
+    async getProductById(id){
+        try {
+            const productos = await this.leerProductos()
+            const productoEncontrado = productos.find(producto => producto.id === id)
+            if (!productoEncontrado){
+                throw new Error('Producto no encontrado')
+            }else{
+                return productoEncontrado;
+            }
+        } catch (error) {
+            console.error("Error al consultar productos", error)
+            return []
+        }
+    
+    }
+
+    async modificarProduct(id, title, description, price, thumbnail, code, stock){
+        try{
+            let productos = await this.leerProductos();
+            const productoEncontrado = productos.find((producto)=> producto.code === code);
+            if (!productoEncontrado){
+                console.error('not found');
+                return;
+            }
             if (!title || !description || !price || !thumbnail || !code || !stock){
                 console.log('debe completar todos los datos');
                 return;
             }
-            const idProducto= this.products.length + 1;
-            const product= {
-                id: idProducto,
-                title, 
-                description, 
-                price, 
-                thumbnail, 
-                code, 
-                stock
-            }
-            this.products.push(product);
-    }
-
-    getProducts(){
-        return this.products;
-    }
-
-    getProductById(id){
-        const ProductoEncontrado = this.products.find((product)=> product.id === id);
-        if (!ProductoEncontrado){
-            console.error('not found');
-            return
+            productoEncontrado.title = title;
+            productoEncontrado.description = description;
+            productoEncontrado.price = price;
+            productoEncontrado.thumbnail = thumbnail;
+            productoEncontrado.code = code;
+            productoEncontrado.stock = stock;
+            await fs.writeFile(this.productsFile, JSON.stringify(productos, null, 2))
+        }catch(error){
+            console.log('error al modificar producto', error);
         }
-        return ProductoEncontrado;
+    }
 
+
+    async eliminarProducto(id){
+        try{
+            let products = await this.leerProductos();
+            const productoEncontrado = products.find((product)=> product.id === id);
+            if (!productoEncontrado){
+                console.error('not found');
+                return;
+            }else{
+                products = products.filter((product)=> product.id !== id);
+                await fs.writeFile(this.productsFile, JSON.stringify(products, null, 2))
+            }
+        }catch(error){
+            console.log('error al eliminar producto', error);
+        }
     }
 }
 
 const ProductManager = new productManager();
 
-ProductManager.addProduct('Don Sturn', 'ricas', 100, 'fotos de don saturn', 'codigo1', 109);
-ProductManager.addProduct('CINDOR', 'manjar', 150, 'fotos de CINDOR', 'codigo2', 100);
-//ProductManager.addProduct('Tita', 'galletitas', 'fotos de tita', 'codigo3', 10);  ejemplo de producto incompleto
-const productos = ProductManager.getProducts();
+    // ProductManager.addProduct({
+    // title: "producto prueba",
+    // description:"Este es un producto prueba",
+    // price:200,
+    // thumbnail:"”Sin imagen”",
+    // code:"abc123",
+    // stock:25
 
-console.log(productos);
-//console.log(ProductManager.getProductById(9));
+    // })
 
+    // ProductManager.getProducts()
+    // .then((productos)=> console.log("Productos :" ,productos))
+    // .catch((error)=> console.log('error al consultar productos', error));
+
+
+    // ProductManager.getProductById(5)
+    // .then((producto)=> console.log("Producto Buscado :", producto))
+    // .catch((error)=> console.log('error al consultar producto', error));
+
+    // ProductManager.modificarProduct(1, "PEPAS!!!!!", "Este es un producto prueba modificado", 300, "”Sin imagen”", "abc123", 25)
+    // .then(()=> console.log("Producto modificado"))
+    // .catch((error)=> console.log('error al modificar producto', error));
+
+    // ProductManager.eliminarProducto(1)
+    // .then(()=> console.log("Producto eliminado"))
+    // .catch((error)=> console.log('error al eliminar producto', error));
