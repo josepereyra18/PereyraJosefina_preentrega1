a7cfc9b3642c2a1166374b00499e9d2e94558490
Josefina Pereyra <j08pereyra@gmail.com>
2024-04-11 19:20:05 -0300

    segunda entrega

--- app.js
+++ app.js
@@ -1,79 +1,82 @@
-const fs = require('fs').promises
+//Corregido!! con this.product
+
+const fs = require('fs').promises;
+
 
 class ProductManager {
     constructor (){
         this.productsFile = 'productos.json';
+        this.leerProductos();
+        this.products = [];
     }
 
     async leerProductos (){
         try{
-            const productos = await fs.readFile(this.productsFile, 'utf8');
-            return JSON.parse(productos);
+            const productos1 = await fs.readFile(this.productsFile, 'utf8');
+            this.products = JSON.parse(productos1); 
         } catch (error){
             if (error.code === 'ENOENT'){
-                return [];
-            }else{
+                this.products = [];
+            } else {
                 throw error;
             }
         }
     }
 
+
     async addProduct(product){
         try {
-            let productos = await this.leerProductos();
-            const codigoEncontrado = productos.find((producto) => producto.code === product.code);
+            const codigoEncontrado = this.products.find((producto) => producto.code === product.code);
             if (codigoEncontrado) {
                 console.log('El codigo ya existe');
                 return;
             }
             if (!product.title || !product.description || !product.price || !product.thumbnail || !product.code || !product.stock) {
-                console.log('debe completar todos los datos');
+                console.log('Debe completar todos los datos');
                 return;
             }
-            const idProducto = productos.length + 1;
+            const idProducto = this.products.length + 1;
             const producto = { ...product, id: idProducto };
-            productos.push(producto);
-            await fs.writeFile(this.productsFile, JSON.stringify(productos, null, 2));
+            this.products.push(producto);
+            await fs.writeFile(this.productsFile, JSON.stringify(this.products, null, 2));
         } catch (error) {
-            console.log('error al agregar producto', error);
+            console.log('Error al agregar producto', error);
         }
     }
 
     async getProducts(){
         try {
-            return await this.leerProductos()
+            return this.products;
         } catch (error) {
-            console.error("Error al consultar productos", error)
-            return []
+            console.error("Error al consultar productos", error);
+            return [];
         }
     }
 
     async getProductById(id){
         try {
-            const productos = await this.leerProductos()
-            const productoEncontrado = productos.find(producto => producto.id === id)
+            const productoEncontrado = this.products.find(producto => producto.id === id);
             if (!productoEncontrado){
-                throw new Error('Producto no encontrado')
-            }else{
+                throw new Error('Producto no encontrado');
+            } else {
                 return productoEncontrado;
             }
         } catch (error) {
-            console.error("Error al consultar productos", error)
-            return []
+            console.error("Error al consultar productos", error);
+            return [];
         }
     
     }
 
     async modificarProduct(id, title, description, price, thumbnail, code, stock){
         try{
-            let productos = await this.leerProductos();
-            const productoEncontrado = productos.find((producto)=> producto.code === code);
+            const productoEncontrado = this.products.find((producto)=> producto.id === id);
             if (!productoEncontrado){
-                console.error('not found');
+                console.error('Not found');
                 return;
             }
             if (!title || !description || !price || !thumbnail || !code || !stock){
-                console.log('debe completar todos los datos');
+                console.log('Debe completar todos los datos');
                 return;
             }
             productoEncontrado.title = title;
@@ -82,48 +85,47 @@ class ProductManager {
             productoEncontrado.thumbnail = thumbnail;
             productoEncontrado.code = code;
             productoEncontrado.stock = stock;
-            await fs.writeFile(this.productsFile, JSON.stringify(productos, null, 2))
-        }catch(error){
-            console.log('error al modificar producto', error);
+            await fs.writeFile(this.productsFile, JSON.stringify(this.products, null, 2));
+        } catch(error){
+            console.log('Error al modificar producto', error);
         }
     }
 
-
     async eliminarProducto(id){
         try{
-            let products = await this.leerProductos();
-            const productoEncontrado = products.find((product)=> product.id === id);
+            const productoEncontrado = this.products.find((product)=> product.id === id);
             if (!productoEncontrado){
-                console.error('not found');
+                console.error('Not found');
                 return;
-            }else{
-                products = products.filter((product)=> product.id !== id);
-                await fs.writeFile(this.productsFile, JSON.stringify(products, null, 2))
+            } else {
+                this.products = this.products.filter((product)=> product.id !== id);
+                await fs.writeFile(this.productsFile, JSON.stringify(this.products, null, 2));
             }
-        }catch(error){
-            console.log('error al eliminar producto', error);
+        } catch(error){
+            console.log('Error al eliminar producto', error);
         }
     }
 }
 
+
 const productManager = new ProductManager();
 
-    // productManager.addProduct({
-    // title: "producto prueba",
-    // description:"Este es un producto prueba",
-    // price:200,
-    // thumbnail:"”Sin imagen”",
-    // code:"456",
-    // stock:25
+    productManager.addProduct({
+    title: "jujuju",
+    description:"Este es un producto prueba",
+    price:105,
+    thumbnail:"”Sin imagen”",
+    code:"j890",
+    stock:25
 
-    // })
+    })
 
     // productManager.getProducts()
     // .then((productos)=> console.log("Productos :" ,productos))
     // .catch((error)=> console.log('error al consultar productos', error));
 
 
-    // productManager.getProductById(5)
+    // productManager.getProductById(1)
     // .then((producto)=> console.log("Producto Buscado :", producto))
     // .catch((error)=> console.log('error al consultar producto', error));
 
